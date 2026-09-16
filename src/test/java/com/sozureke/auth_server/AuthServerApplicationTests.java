2dfe8888da6cfee4b050b06a0e507f6f9e6dacfe
Marshanishvili Mykhailo <sozure.synergy@gmail.com>
2026-09-16 14:45:17 +0200

    chore(init): bootstrap Spring Boot skeleton with Docker, Flyway and env-based config

---
+++ src/test/java/com/sozureke/auth_server/AuthServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.sozureke.auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
